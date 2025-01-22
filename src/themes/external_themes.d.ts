de612c0d3b706313bf95dd8a768f913d00420a83
Alan Smith <alan@randomsmiths.com>
2025-01-22 06:28:48 -0500

    Add license headers

--- src/themes/external_themes.d.ts
+++ src/themes/external_themes.d.ts
@@ -16,20 +16,7 @@
  */
 
 /**
- * @fileoverview Light theme
+ * @fileoverview External themes (make typescript declarations)
  * @author alan@porpoiseful.com (Alan Smith)
  */
-import * as Blockly from 'blockly/core'
-import { add_mrc_styles } from './styles';
-
-const THEME_NAME = 'mrc_theme_light';
-
-const create_theme = function() : Blockly.Theme{    
-   let newTheme = Blockly.Theme.defineTheme(THEME_NAME, {
-        name: THEME_NAME,
-        base: Blockly.Themes.Classic,
-    });
-    return add_mrc_styles(newTheme);
-}
-
-export const theme = create_theme();
+declare module '@blockly/theme-dark';
