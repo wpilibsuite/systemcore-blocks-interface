ce3c11974ffc19b58e4047067e732ce3d7e6f087
Alan Smith <alan@randomsmiths.com>
2025-03-10 17:07:38 -0400

    Changed str in version to be an empty string

--- external_samples/rev_touch_sensor.py
+++ external_samples/rev_touch_sensor.py
@@ -12,7 +12,7 @@ class RevTouchSensor(Component):
     def get_url(self) -> str:  
         return "https://www.revrobotics.com/rev-31-1425/"
     def get_version(self) -> tuple[int, int, str]: 
-        return (1, 0, None)
+        return (1, 0, "")
     def stop(self) -> None:
         pass
     def reset(self) -> None:
