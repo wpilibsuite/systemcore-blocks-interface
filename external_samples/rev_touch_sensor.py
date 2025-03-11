6a363713f3cb4cc5e32513441db516a7eee0aded
Alan Smith <alan@randomsmiths.com>
2025-03-11 19:29:48 -0400

    Add callback and docstrings

--- external_samples/rev_touch_sensor.py
+++ external_samples/rev_touch_sensor.py
@@ -2,6 +2,9 @@ from component import Component, PortType
 from _collections_abc import Callable
 
 class RevTouchSensor(Component):
+    def __init__(self):
+        self.is_pressed = None
+
     # Required methods
     def get_manufacturer(self) -> str:   
         return "REV Robotics" 
@@ -20,19 +23,27 @@ class RevTouchSensor(Component):
     def get_connection_port_type(self) -> list[PortType]:
         return [PortType.SMART_IO_PORT]
     def periodic(self) -> None:
-        # This would poll the hardware and see if it needs to fire any events
+        old = self.is_pressed
+        self._read_hardware()
+        if old != self.is_pressed:
+            if self.is_pressed and self.pressed_callback:
+                self.pressed_callback()
+            elif old and self.released_callback:
+                self.released_callback()
+    def _read_hardware(self):
+        # here read hardware to get the current value of the sensor and set self.is_pressed
         pass
 
-    # Methods
     def is_pressed(self) -> bool:
-        # Code to communicate using WPILib would go here
-        return True
+        '''Returns if the touch sensor is pressed or not'''
+        return self.is_pressed
     
     # Events
-    def register_when_pressed(callback: Callable[[], None]) -> None:
-        # Code to register callback here
-        pass
+    def register_when_pressed(self, callback: Callable[[], None]) -> None:
+        '''Event when touch sensor is first pressed'''
+        self.pressed_callback = callback
 
-    def register_when_released(callback: Callable[[], None]) -> None:
-        # Code to register callback here
-        pass
+
+    def register_when_released(self, callback: Callable[[], None]) -> None:
+        '''Event when touch sensor is first released'''
+        self.released_callback = callback
