2e78c2978a6d8067a76b490bc53f22fb01c02805
Alan Smith <alan@randomsmiths.com>
2025-06-02 19:24:23 -0400

    Add in Logo

--- src/reactComponents/Header.tsx
+++ src/reactComponents/Header.tsx
@@ -24,25 +24,27 @@ import * as Antd from 'antd';
 type StringFunctionVoid = (input: string) => void;
 
 interface HeaderProps {
-    alertErrorMessage : string;
-    setAlertErrorMessage : StringFunctionVoid;
+  alertErrorMessage: string;
+  setAlertErrorMessage: StringFunctionVoid;
 }
 
-export default function Header(props : HeaderProps) {
-    return (
-       <Antd.Flex vertical style={{background: '#000'}}>
-          <Antd.Typography.Paragraph
-            strong={true}
-            style={{color: '#aaf' }}
-          >
-            WPILib Blocks
-          </Antd.Typography.Paragraph>
-          {props.alertErrorMessage !== '' && (<Antd.Alert
-            type="error"
-            message={props.alertErrorMessage}
-            closable
-            afterClose={() => props.setAlertErrorMessage('')}
-          />)}
-        </Antd.Flex>
-    )
+export default function Header(props: HeaderProps) {
+  return (
+    <Antd.Flex vertical style={{ background: '#000' }}>
+      <Antd.Flex style={{alignItems: "center"}}>
+        <img height="30px" style={{ objectFit: "contain" }} src="/FIRST_HorzRGB_reverse.png" />
+        <Antd.Typography
+          style={{ color: 'white', paddingLeft: 20, fontWeight: "bolder"}}
+        >
+          Blocks
+        </Antd.Typography>
+        {props.alertErrorMessage !== '' && (<Antd.Alert
+          type="error"
+          message={props.alertErrorMessage}
+          closable
+          afterClose={() => props.setAlertErrorMessage('')}
+        />)}
+      </Antd.Flex>
+    </Antd.Flex>
+  )
 }
