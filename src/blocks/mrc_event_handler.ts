d6fe7395ab78da7d84681773b1f9f449b3314467
Alan Smith <alan@randomsmiths.com>
2025-06-23 10:44:36 -0400

    Used copilot to follow google coding guidelines

--- src/blocks/mrc_event_handler.ts
+++ src/blocks/mrc_event_handler.ts
@@ -19,206 +19,204 @@
  * @fileoverview Blocks for event handlers
  * @author alan@porpoiseful.com (Alan Smith)
  */
+
 import * as Blockly from 'blockly';
-import { MRC_STYLE_EVENT_HANDLER } from '../themes/styles';
-import { createFieldNonEditableText } from '../fields/FieldNonEditableText'
-import { createFieldFlydown } from '../fields/field_flydown';
-import { Order } from 'blockly/python';
-import { ExtendedPythonGenerator } from '../editor/extended_python_generator';
+import {Order} from 'blockly/python';
+
+import {ExtendedPythonGenerator} from '../editor/extended_python_generator';
+import {createFieldFlydown} from '../fields/field_flydown';
+import {createFieldNonEditableText} from '../fields/FieldNonEditableText';
+import {MRC_STYLE_EVENT_HANDLER} from '../themes/styles';
 
 export const BLOCK_NAME = 'mrc_event_handler';
 
 export enum SenderType {
-    ROBOT = 'robot',
-    MECHANISM = 'mechanism',
-    COMPONENT = 'component'
+  ROBOT = 'robot',
+  MECHANISM = 'mechanism',
+  COMPONENT = 'component'
 }
 
-export type Parameter = {
-    name: string,
-    type?: string,
-};
+export interface Parameter {
+  name: string;
+  type?: string;
+}
 
 export type EventHandlerBlock = Blockly.Block & EventHandlerMixin & Blockly.BlockSvg;
+
 interface EventHandlerMixin extends EventHandlerMixinType {
-    mrcPathOfSender: string,
-    mrcTypeOfSender: SenderType, 
-    mrcParameters: Parameter[],
+  mrcPathOfSender: string;
+  mrcTypeOfSender: SenderType;
+  mrcParameters: Parameter[];
 }
+
 type EventHandlerMixinType = typeof EVENT_HANDLER;
 
-
-/** Extra state for serialising call_python_* blocks. */
-export type EventHandlerExtraState = {
-    pathOfSender: string,
-    typeOfSender: SenderType,  
-
-    /** The parameters of the event handler. */
-    params: Parameter[],
-};
+/** Extra state for serialising event handler blocks. */
+export interface EventHandlerExtraState {
+  pathOfSender: string;
+  typeOfSender: SenderType;
+  /** The parameters of the event handler. */
+  params: Parameter[];
+}
 
 const EVENT_HANDLER = {
-    /**
-     * Block initialization.
-     */
-    init: function (this: EventHandlerBlock): void {
-        this.appendDummyInput("TITLE")
-            .appendField('When')
-            .appendField(createFieldNonEditableText('sender'), 'SENDER')
-            .appendField(createFieldNonEditableText('eventName'), 'EVENT_NAME');
-        this.appendDummyInput('PARAMS')
-            .appendField('with')
-        this.setOutput(false);
-        this.setStyle(MRC_STYLE_EVENT_HANDLER);
-        this.appendStatementInput('STACK').appendField('');
-        this.mrcParameters = [];
-        this.setPreviousStatement(false);
-        this.setNextStatement(false);
-    },
-    /**
-       * Returns the state of this block as a JSON serializable object.
-       */
-    saveExtraState: function (
-        this: EventHandlerBlock): EventHandlerExtraState {
-        const extraState: EventHandlerExtraState = {
-            pathOfSender: this.mrcPathOfSender,
-            typeOfSender: this.mrcTypeOfSender, 
-            params: [],
-        };
+  /**
+   * Block initialization.
+   */
+  init(this: EventHandlerBlock): void {
+    this.appendDummyInput('TITLE')
+        .appendField('When')
+        .appendField(createFieldNonEditableText('sender'), 'SENDER')
+        .appendField(createFieldNonEditableText('eventName'), 'EVENT_NAME');
+    this.appendDummyInput('PARAMS')
+        .appendField('with');
+    this.setOutput(false);
+    this.setStyle(MRC_STYLE_EVENT_HANDLER);
+    this.appendStatementInput('STACK').appendField('');
+    this.mrcParameters = [];
+    this.setPreviousStatement(false);
+    this.setNextStatement(false);
+  },
+
+  /**
+   * Returns the state of this block as a JSON serializable object.
+   */
+  saveExtraState(this: EventHandlerBlock): EventHandlerExtraState {
+    const extraState: EventHandlerExtraState = {
+      pathOfSender: this.mrcPathOfSender,
+      typeOfSender: this.mrcTypeOfSender,
+      params: [],
+    };
+    
+    this.mrcParameters.forEach((param) => {
+      extraState.params.push({
+        name: param.name,
+        type: param.type,
+      });
+    });
+
+    return extraState;
+  },
+
+  /**
+   * Applies the given state to this block.
+   */
+  loadExtraState(this: EventHandlerBlock, extraState: EventHandlerExtraState): void {
+    this.mrcParameters = [];
+    this.mrcPathOfSender = extraState.pathOfSender;
+    this.mrcTypeOfSender = extraState.typeOfSender;
+
+    extraState.params.forEach((param) => {
+      this.mrcParameters.push({
+        name: param.name,
+        type: param.type,
+      });
+    });
+    this.mrcUpdateParams();
+  },
+
+  /**
+   * Update the block to reflect the newly loaded extra state.
+   */
+  mrcUpdateParams(this: EventHandlerBlock): void {
+    if (this.mrcParameters.length > 0) {
+      const input = this.getInput('PARAMS');
+      if (input) {
+        this.removeParameterFields(input);
         this.mrcParameters.forEach((param) => {
-            extraState.params.push({
-                'name': param.name,
-                'type': param.type,
-            });
+          const paramName = `PARAM_${param.name}`;
+          input.appendField(createFieldFlydown(param.name, false), paramName);
         });
+      }
+    } else {
+      this.removeInput('PARAMS', true);
+    }
+  },
 
-        return extraState;
-    },
-    /**
-     * Applies the given state to this block.
-     */
-    loadExtraState: function (
-        this: EventHandlerBlock,
-        extraState: EventHandlerExtraState
-    ): void {
-        this.mrcParameters = [];
-        this.mrcPathOfSender = extraState.pathOfSender;
-        this.mrcTypeOfSender = extraState.typeOfSender;
+  /**
+   * Removes parameter fields from the given input.
+   */
+  removeParameterFields(input: Blockly.Input): void {
+    const fieldsToRemove = input.fieldRow
+        .filter(field => field.name?.startsWith('PARAM_'))
+        .map(field => field.name!);
 
-        extraState.params.forEach((param) => {
-            this.mrcParameters.push({
-                'name': param.name,
-                'type': param.type,
-            });
-        });
-        this.mrcUpdateParams();
-    },
-    /**
-     * Update the block to reflect the newly loaded extra state.
-     */
-    mrcUpdateParams: function (this: EventHandlerBlock) {
-        if (this.mrcParameters.length > 0) {
-            let input = this.getInput('PARAMS');
-            if (input) {
-                this.removeParameterFields(input);
-                this.mrcParameters.forEach((param) => {
-                    const paramName = 'PARAM_' + param.name;                    
-                    input.appendField(createFieldFlydown(param.name, false), paramName);
-                });
-            }
-        }else{
-            this.removeInput('PARAMS', true);
-        }
-    },
-    removeParameterFields: function (input: Blockly.Input) {
-        const fieldsToRemove = input.fieldRow
-            .filter(field => field.name?.startsWith('PARAM_'))
-            .map(field => field.name!);
-
-        fieldsToRemove.forEach(fieldName => {
-            input.removeField(fieldName);
-        });
-    },
-
+    fieldsToRemove.forEach(fieldName => {
+      input.removeField(fieldName);
+    });
+  },
 };
 
+export function setup(): void {
+  Blockly.Blocks[BLOCK_NAME] = EVENT_HANDLER;
+}
 
-export const setup = function () {
-    Blockly.Blocks[BLOCK_NAME] = EVENT_HANDLER;
-};
-
-export const pythonFromBlock = function (
+export function pythonFromBlock(
     block: EventHandlerBlock,
     generator: ExtendedPythonGenerator,
-) {
-    const blocklyName = block.getFieldValue('SENDER') + '_' + block.getFieldValue('EVENT_NAME');
+): string {
+  const blocklyName = `${block.getFieldValue('SENDER')}_${block.getFieldValue('EVENT_NAME')}`;
+  const funcName = generator.getProcedureName(blocklyName);
 
-    const funcName = generator.getProcedureName(blocklyName);
+  let xfix1 = '';
+  if (generator.STATEMENT_PREFIX) {
+    xfix1 += generator.injectId(generator.STATEMENT_PREFIX, block);
+  }
+  if (generator.STATEMENT_SUFFIX) {
+    xfix1 += generator.injectId(generator.STATEMENT_SUFFIX, block);
+  }
+  if (xfix1) {
+    xfix1 = generator.prefixLines(xfix1, generator.INDENT);
+  }
 
-    let xfix1 = '';
-    if (generator.STATEMENT_PREFIX) {
-        xfix1 += generator.injectId(generator.STATEMENT_PREFIX, block);
-    }
-    if (generator.STATEMENT_SUFFIX) {
-        xfix1 += generator.injectId(generator.STATEMENT_SUFFIX, block);
-    }
-    if (xfix1) {
-        xfix1 = generator.prefixLines(xfix1, generator.INDENT);
-    }
-    let loopTrap = '';
-    if (generator.INFINITE_LOOP_TRAP) {
-        loopTrap = generator.prefixLines(
-            generator.injectId(generator.INFINITE_LOOP_TRAP, block),
-            generator.INDENT,
-        );
-    }
-    let branch = '';
-    if (block.getInput('STACK')) {
-        branch = generator.statementToCode(block, 'STACK');
-    }
-    let returnValue = '';
-    if (block.getInput('RETURN')) {
-        returnValue = generator.valueToCode(block, 'RETURN', Order.NONE) || '';
-    }
-    let xfix2 = '';
-    if (branch && returnValue) {
-        // After executing the function body, revisit this block for the return.
-        xfix2 = xfix1;
-    }
-    
-    if (returnValue) {
-        returnValue = generator.INDENT + 'return ' + returnValue + '\n';
-    } else if (!branch) {
-        branch = generator.PASS;
-    }
+  let loopTrap = '';
+  if (generator.INFINITE_LOOP_TRAP) {
+    loopTrap = generator.prefixLines(
+        generator.injectId(generator.INFINITE_LOOP_TRAP, block),
+        generator.INDENT,
+    );
+  }
 
-    let params = block.mrcParameters;
-    let paramString = "self";
+  let branch = '';
+  if (block.getInput('STACK')) {
+    branch = generator.statementToCode(block, 'STACK');
+  }
 
-    if (params.length != 0) {
-        block.mrcParameters.forEach((param) => {
-            paramString += ', ' + param.name;
-        });
-    }
+  let returnValue = '';
+  if (block.getInput('RETURN')) {
+    returnValue = generator.valueToCode(block, 'RETURN', Order.NONE) || '';
+  }
 
-    let code = 'def ' +
-        funcName +
-        '(' +
-        paramString +
-        '):\n';
+  let xfix2 = '';
+  if (branch && returnValue) {
+    // After executing the function body, revisit this block for the return.
+    xfix2 = xfix1;
+  }
 
-    code +=
-        xfix1 +
-        loopTrap +
-        branch +
-        xfix2 +
-        returnValue;
-    code = generator.scrub_(block, code);
-    generator.addClassMethodDefinition(funcName, code);
-    generator.addEventHandler(
-        block.getFieldValue('SENDER'),
-        block.getFieldValue('EVENT_NAME'),
-        funcName);
+  if (returnValue) {
+    returnValue = `${generator.INDENT}return ${returnValue}\n`;
+  } else if (!branch) {
+    branch = generator.PASS;
+  }
 
-    return ''
+  const params = block.mrcParameters;
+  let paramString = 'self';
+
+  if (params.length !== 0) {
+    block.mrcParameters.forEach((param) => {
+      paramString += `, ${param.name}`;
+    });
+  }
+
+  let code = `def ${funcName}(${paramString}):\n`;
+  code += xfix1 + loopTrap + branch + xfix2 + returnValue;
+  code = generator.scrub_(block, code);
+  
+  generator.addClassMethodDefinition(funcName, code);
+  generator.addEventHandler(
+      block.getFieldValue('SENDER'),
+      block.getFieldValue('EVENT_NAME'),
+      funcName);
+
+  return '';
 }
