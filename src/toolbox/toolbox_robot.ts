96f7ff75f65d7c39d4221af313a2ecc260eccad6
Alan Smith <alan412@users.noreply.github.com>
2025-06-23 12:28:38 -0400

    Add event category to robot as well (#125)

--- src/toolbox/toolbox_robot.ts
+++ src/toolbox/toolbox_robot.ts
@@ -1,5 +1,6 @@
  import * as common from './toolbox_common'
  import {category as hardwareCategory} from './hardware_category';
+ import {category as eventCategory} from './event_category';
  
  export function getToolboxJSON(
      shownPythonToolboxCategories: Set<string> | null) {
@@ -8,6 +9,7 @@
          kind: 'categoryToolbox',
          contents: [
             hardwareCategory,
+            eventCategory,
             { kind: 'sep' },
             ...common.getToolboxItems(shownPythonToolboxCategories)
          ]
