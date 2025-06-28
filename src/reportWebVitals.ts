c08e9122d653bdb5231c56b96beb151e7d84c5ca
Alan Smith <alan@randomsmiths.com>
2025-06-28 08:56:29 -0400

    Fixed missing parenthesis

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -6,7 +6,7 @@ const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
       onFCP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
-  }
+  });
 };
 
 export default reportWebVitals;
