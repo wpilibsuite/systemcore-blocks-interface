976d5d386adb5d3c42a0c76796fe3772c18c80c2
Alan Smith <alan@randomsmiths.com>
2025-07-03 06:58:16 -0400

    Robot now shows ports for a component

--- src/toolbox/hardware_category.ts
+++ src/toolbox/hardware_category.ts
@@ -49,12 +49,12 @@ export function getHardwareCategory(currentModule: commonStorage.Module) {
       name: 'Hardware',
       contents: [
         getRobotMechanismsBlocks(currentModule),
-        getComponentsBlocks(currentModule),
+        getComponentsBlocks(currentModule, false),
       ]
     };
   }
   if (currentModule.moduleType === commonStorage.MODULE_TYPE_MECHANISM) {
-    return getComponentsBlocks(currentModule);
+    return getComponentsBlocks(currentModule, true);
   }
   // Return default empty category if module type doesn't match
   return {
@@ -239,14 +239,14 @@ function getRobotMethodsBlocks(currentModule: commonStorage.Module) {
   };
 }
 
-function getComponentsBlocks(currentModule: commonStorage.Module) {
+function getComponentsBlocks(currentModule: commonStorage.Module, hideParams : boolean) {
   const contents = [];
 
   // Add the "+ Component" category
   contents.push({
     kind: 'category',
     name: '+ Component',
-    contents: getAllPossibleComponents(true)
+    contents: getAllPossibleComponents(hideParams)
   });
 
   // Get components from the current workspace
