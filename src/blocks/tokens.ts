043670acf283b668c81ae82902f4483195fdfa2f
Liz Looney <lizlooney@gmail.com>
2025-07-18 21:39:14 -0700

    Removed ADD_COMMENT, REMOVE_COMMENT, and DUPLICATE_COMMENT from tokens.ts.

--- src/blocks/tokens.ts
+++ src/blocks/tokens.ts
@@ -29,9 +29,6 @@ import * as Blockly from 'blockly/core';
  */
 export function customTokens(t: (key: string) => string): typeof Blockly.Msg {
   return {
-    ADD_COMMENT: t('BLOCKLY.ADD_COMMENT'),
-    REMOVE_COMMENT: t('BLOCKLY.REMOVE_COMMENT'),
-    DUPLICATE_COMMENT: t('BLOCKLY.DUPLICATE_COMMENT'),
     OF_TYPE: t('BLOCKLY.OF_TYPE'),
     WITH: t('BLOCKLY.WITH'),
     WHEN: t('BLOCKLY.WHEN'),
