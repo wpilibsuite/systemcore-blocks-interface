439b8866f8592180e93a6dfb976dd1fe635c7423
Liz Looney <lizlooney@gmail.com>
2025-08-29 05:32:15 -0700

    Remove temporary code. (#213)

--- src/storage/client_side_storage.ts
+++ src/storage/client_side_storage.ts
@@ -20,8 +20,6 @@
  */
 
 import * as commonStorage from './common_storage';
-import * as storageModuleContent from './module_content';
-import * as storageNames from './names';
 
 // Functions for saving blocks files to client side storage.
 
@@ -58,62 +56,7 @@ export async function openClientSideStorage(): Promise<commonStorage.Storage> {
     };
     openRequest.onsuccess = () => {
       const db = openRequest.result;
-      fixOldModules(db).then(() => {
-        resolve(ClientSideStorage.create(db));
-      })
-    };
-  });
-}
-
-// The following function allows Alan and Liz to load older projects.
-// TODO(lizlooney): Remove this function.
-async function fixOldModules(db: IDBDatabase): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction([FILES_STORE_NAME], 'readwrite');
-    transaction.oncomplete = () => {
-      resolve();
-    };
-    transaction.onabort = () => {
-      console.log('IndexedDB transaction aborted.');
-      reject(new Error('IndexedDB transaction aborted.'));
-    };
-    const filesObjectStore = transaction.objectStore(FILES_STORE_NAME);
-    const openCursorRequest = filesObjectStore.openCursor();
-    openCursorRequest.onerror = () => {
-      console.log('IndexedDB openCursor request failed. openCursorRequest.error is...');
-      console.log(openCursorRequest.error);
-      reject(new Error('IndexedDB openCursor request failed.'));
-    };
-    openCursorRequest.onsuccess = () => {
-      const cursor = openCursorRequest.result;
-      if (cursor) {
-        const value = cursor.value;
-        const regexForOldModulePath = new RegExp('^([A-Z][A-Za-z0-9]*)/([A-Z][A-Za-z0-9]*).json$');
-        const result = regexForOldModulePath.exec(value.path);
-        if (result) {
-          const oldModulePath = value.path;
-          const projectName = result[1];
-          const className = result[2];
-          const moduleType = storageModuleContent.parseModuleContentText(value.content).getModuleType();
-          value.path = storageNames.makeModulePath(projectName, className, moduleType);
-          const putRequest = filesObjectStore.put(value);
-          putRequest.onerror = () => {
-           console.log('IndexedDB put request failed. putRequest.error is...');
-            console.log(putRequest.error);
-            throw new Error('IndexedDB put request failed.');
-          };
-          const deleteRequest = filesObjectStore.delete(oldModulePath);
-          deleteRequest.onerror = () => {
-            console.log('IndexedDB delete request failed. deleteRequest.error is...');
-            console.log(deleteRequest.error);
-            throw new Error('IndexedDB delete request failed.');
-          };
-        }
-        cursor.continue();
-      } else {
-        // The cursor is done. We have finished reading all the files.
-        resolve();
-      }
+      resolve(ClientSideStorage.create(db));
     };
   });
 }
