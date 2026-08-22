34867c4bd1741605de6bd805b5d5538ad515c8d4
Alan Smith <alan@randomsmiths.com>
2026-08-22 11:32:04 -0400

    Change to use method in mrc_get_python_variable instead of repeating code.

--- frontend/toolbox/driver_station_category.ts
+++ frontend/toolbox/driver_station_category.ts
@@ -3,7 +3,7 @@ import * as Blockly from 'blockly/core';
 import * as toolboxItems from './items';
 import { Editor } from '../editor/editor';
 import { addStaticMethodBlocks } from '../blocks/mrc_call_python_function';
-import { BLOCK_NAME as MRC_GET_PYTHON_VARIABLE } from '../blocks/mrc_get_python_variable';
+import { createModuleOrClassVariableGetterBlock, VariableKind } from '../blocks/mrc_get_python_variable';
 import { makeOneContents } from './robotpy_toolbox';
 import { CLASS_NAME_DRIVER_STATION_DISPLAY, getClassData } from '../blocks/utils/python';
 import { BLOCK_NAME as MRC_GAMEPAD_BOOLEAN  } from '../blocks/mrc_gamepad_boolean';
@@ -55,31 +55,15 @@ function plugDefaultColorBlocks(blocks: toolboxItems.ContentsType[]) {
         }
         const colorArgIndex = args.findIndex(arg => arg.type === 'wpiutil.Color');
         if (colorArgIndex !== -1) {
-            block.inputs['ARG' + colorArgIndex] = createDefaultColorBlock();
+            // Plug in a real (non-shadow) wpiutil.Color.WHITE class variable getter, so
+            // users can just change the color using the dropdown rather than needing to
+            // find and attach the block themselves.
+            block.inputs['ARG' + colorArgIndex] = {
+                block: createModuleOrClassVariableGetterBlock(
+                    VariableKind.CLASS, 'wpiutil', 'wpiutil.Color', 'wpiutil.Color', 'WHITE', false),
+            };
         }
     });
-}
-
-// Creates a real (non-shadow) block for the color input, plugged in as a
-// wpiutil.Color.WHITE class variable getter, so users can just change the
-// color using the dropdown rather than needing to find and attach the block
-// themselves.
-function createDefaultColorBlock(): {[key: string]: any} {
-    return {
-        block: {
-            type: MRC_GET_PYTHON_VARIABLE,
-            extraState: {
-                varKind: 'class',
-                moduleOrClassName: 'wpiutil.Color',
-                varType: 'wpiutil.Color',
-                importModule: 'wpiutil',
-            },
-            fields: {
-                'MODULE_OR_CLASS': 'wpiutil.Color',
-                'VAR': 'WHITE',
-            },
-        },
-    };
 }
 
 function getDriverStationGamepadsCategory(_editor: Editor): toolboxItems.Category {
